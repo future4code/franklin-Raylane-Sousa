14520e372266a815a3cbc260d30eec30eec446d3
raylane <raylane.ncs@gmail.com>
2022-08-10 12:45:48 -0300

    intro API e Express

---
+++ modulo3/intro-express/Intro-ApiExpress/intro-api-express/src/database/posts.ts
@@ -0,0 +1,27 @@
+//Exercício 05/06
+export const posts: any = [
+    {
+        "id": 1,
+        "title": "This is Real",
+        "body": "The sky is blue",
+        "userId": 1
+    },
+    {
+        "id": 2,
+        "title": "The night is darker",
+        "body": "The sky is darkblue",
+        "userId": 1
+    },
+    {
+        "id": 3,
+        "title": "No pain no gain",
+        "body": "Nobody's knows",
+        "userId": 2
+    },
+    {
+        "id": 4,
+        "title": "Look at This",
+        "body": "This is me",
+        "userId": 3
+    }
+]
