703134c25594476ca0c89c9be93cdf2dc942607a
raylane <raylane.ncs@gmail.com>
2022-08-25 19:28:33 -0300

    Labebank

---
+++ modulo3/Labebank/Labebank/src/database/db.ts
@@ -0,0 +1,63 @@
+//EXERCICIO03
+
+export const labeAccountClient: any = [
+    {
+        "idClient": "1",
+        "name": "Jane Doe",
+        "cpf": "09812345678",
+        "birthDate": "1995-09-10",
+        "valueA": 1000.000,
+        "extract": [
+            {
+                "id": 1,
+                "value": 55.00,
+                "date": "2022-05-02",
+                "description": "transação realizada"
+            },
+            {
+                "id": 2,
+                "value": 300.00,
+                "purchaseDate": "2022-07-23",
+                "description": "transação realizada"
+            }
+        ]
+    },
+    {
+        "idClient": "2",
+        "name": "John Doe",
+        "cpf": "567438291011",
+        "birthDate": "1994-04-27",
+        "valueA": 1000.000,
+        "extract": [
+            {
+                "value": 45.00,
+                "date": "2022-02-09",
+                "description": "transação realizada"
+            },
+            {
+                "value": 150.00,
+                "purchaseDate": "2022-01-12",
+                "description": "transação realizada"
+            }
+        ]
+    }, 
+    {
+        "idClient": "3",
+        "name": "Dr Botica",
+        "cpf": "09812344432",
+        "birthDate": "1994-06-07",
+        "valueA": 1500.000,
+        "extract": [
+            {
+                "value": 25.00,
+                "date": "2022-02-08",
+                "description": "transação realizada"
+            },
+            {
+                "value": 100.00,
+                "purchaseDate": "2022-04-09",
+                "description": "transação realizada"
+            }
+        ]
+    }  
+] 
---
+++ modulo3/Labebank/Labebank/src/index.ts
@@ -0,0 +1,101 @@
+import express, { Response, Request, response } from "express";
+import cors from "cors";
+import { labeAccountClient } from './database/db'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//VARIAVEIS TIPADAS
+type clientAccount = {
+    idClient: number,
+    name: string,
+    cpf: string,
+    birthDate: string,
+    valueA: number,
+    extract: Array<clientExtract>
+}
+
+type clientExtract = {
+    id: number,
+    value: number,
+    date: string,
+    description: string
+}
+
+//RETORNA TODAS AS INFORMAÇÕES DAS CONTAS
+app.get("/clients", (req:Request, res:Response) => {
+    res.status(200).send(labeAccountClient)
+})
+
+//RETORNA TODOS OS EXTRATOS
+app.get('/extract/', (req:Request, res:Response) => { 
+const extract = labeAccountClient.map((account: any) => {
+    return account.extract
+}).flat(1);
+
+res.status(200).send(extract);
+})
+
+//CRIA CONTA || APENAS MAIOR DE 18 ANOS E COM CPF NÃO CADASTRADO
+app.post('/createaccount', (req: Request, res: Response) =>{
+    const {idClient, name, cpf, birthDate} = req.body
+    const extract: clientExtract[] = []
+    const newLabeAccount: clientAccount = {idClient, name, cpf, birthDate, valueA: 0, extract}
+    
+    const dateBirth: Date = new Date(newLabeAccount.birthDate)
+    const dateToday: Date = new Date();
+    /* calculo da idade */
+    const olderAge: any = dateToday.getFullYear() - dateBirth.getFullYear()
+   /*  verfica o cpf */
+    labeAccountClient.find((client: any) => {if (client.cpf === newLabeAccount.cpf) {
+        res.status(400).send({ message: "O cliente ja possui cadastro!" })
+    }})
+    /* verifica a idade */
+    if (olderAge >= 18) {
+        labeAccountClient.push(newLabeAccount) 
+    } else {
+        res.status(401).send({ message: "O cliente precisa ter mais de 18 anos de idade para abrir a conta!" })
+    }
+
+    res.status(200).send(labeAccountClient)
+}) 
+
+//VERIFICA SALDO PASSANDO NOME E CPF
+app.get("/value", (req: Request, res: Response) => {
+    const { name, cpf } = req.body;
+    if (name && cpf) {
+        const takeValueA = labeAccountClient.find((client: any) => client.name === name && client.cpf === cpf);
+        if (takeValueA) {
+            res.status(200).send({ Cliente: takeValueA.name, Saldo: takeValueA.valueA });
+        } else {
+            res.status(404).send({ message: "Registro não encontrado, verifique se as informações estão corretas!" })
+        }
+    } else {
+        res.status(400).send({ message: "Informe o nome e o cpf!"});
+    }
+});
+
+//ADICIONAR SALDO PASSANDO NOME, CPF e VALOR
+app.post('/value/', (req: Request, res: Response) =>{
+    const {name, cpf, valueA} = req.body
+    const postValueA = labeAccountClient.filter((client: clientAccount) => {
+            return client.name === name && client.cpf === cpf
+     });
+
+    if (postValueA) {
+        postValueA[0].valueA += Number(valueA)
+        res.status(202).send(labeAccountClient);
+    } else{
+        res.status(401).send({"message": "CPF não encontrado, verifique as informações!"})
+    }     
+    
+}) 
+
+
+//Verificando funcionamento do servidor 
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003")
+});
+
